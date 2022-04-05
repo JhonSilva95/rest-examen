4318ea263eb8f6ab3e30d94bf837d93aec91bd94
Jhon Silva <jhon.sicam95@gmail.com>
2022-04-05 18:37:59 -0500

    Estructura proyecto

---
+++ src/main/java/com/fonyou/examen/FonyouExamenApplication.java
@@ -0,0 +1,13 @@
+package com.fonyou.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FonyouExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FonyouExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fonyou/examen/FonyouExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonyou.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FonyouExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
